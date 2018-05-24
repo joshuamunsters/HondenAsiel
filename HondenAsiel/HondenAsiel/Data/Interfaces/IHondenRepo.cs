36d4d08846da945f0fc1fbd3930f711babe81669
Joshua <joshua.munsters@student.fontys.nl>
2018-05-24 14:14:13 +0200

    Honden interface gemaakt

---
+++ HondenAsiel/HondenAsiel/Data/Interfaces/IHondenRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HondenAsiel.Data.Models;
+
+namespace HondenAsiel.Data.Interfaces
+{
+    interface IHondenRepo
+    {
+        IEnumerable<Honden> honden { get; set; }
+
+        IEnumerable<Honden> Gewildehonden { get; set; }
+
+        Honden GethondenbyId(int HondId);
+
+    
+    }
+}
