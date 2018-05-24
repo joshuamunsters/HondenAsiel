08fa34c7954f3b7060606b060151f7da6fbb849d
Joshua <joshua.munsters@student.fontys.nl>
2018-05-24 19:49:30 +0200

    Startup.cs geconfigureerd

--- HondenAsiel/HondenAsiel/Startup.cs
+++ HondenAsiel/HondenAsiel/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using HondenAsiel.Data.Interfaces;
+using HondenAsiel.Data.Mocks;
 
 namespace HondenAsiel
 {
@@ -15,6 +17,8 @@ namespace HondenAsiel
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IHondenRepo, MockHondenRepo>();
+            services.AddTransient<IRasRepo, MockRasRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
