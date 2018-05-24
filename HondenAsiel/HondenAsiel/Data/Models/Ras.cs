92941006e3b7ffa4794589d98870517654186f32
Joshua <joshua.munsters@student.fontys.nl>
2018-05-24 13:56:01 +0200

    Honden en Ras classes gemaakt

--- HondenAsiel/HondenAsiel/Data/Models/Ras.cs
+++ HondenAsiel/HondenAsiel/Data/Models/Ras.cs
@@ -7,5 +7,12 @@ namespace HondenAsiel.Data.Models
 {
     public class Ras
     {
+        public int RasId { get; set; }
+
+        public string Rasnaam { get; set; }
+
+        public string Beschrijving { get; set; }
+
+        public List<Honden> honden { get; set; }
     }
 }
