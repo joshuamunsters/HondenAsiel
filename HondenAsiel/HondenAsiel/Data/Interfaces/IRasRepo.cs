1c85c9c2932c59cf5d46c780c25ead068afc4185
Joshua <joshua.munsters@student.fontys.nl>
2018-05-24 14:17:53 +0200

    Ras interface gemaakt

---
+++ HondenAsiel/HondenAsiel/Data/Interfaces/IRasRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HondenAsiel.Data.Models;
+
+namespace HondenAsiel.Data.Interfaces
+{
+    interface IRasRepo
+    {
+        IEnumerable<Ras> Rassen { get; set; }
+
+    }
+}
