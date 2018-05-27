d0c13d50e34a4750c529d18332ba5f34eeb73e04
Joshua <joshua.munsters@student.fontys.nl>
2018-05-27 19:35:44 +0200

    RasRepo toegevoegd

---
+++ HondenAsiel/HondenAsiel/Data/Repositories/RasRepo.cs
@@ -0,0 +1,21 @@
+﻿using HondenAsiel.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HondenAsiel.Data.Models;
+
+namespace HondenAsiel.Data.Repositories
+{
+    public class RasRepo:IRasRepo
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public RasRepo(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Ras> Rassen => _appDbContext.ras;
+    }
+}
