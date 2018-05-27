8eddfc52a54dfbe37a62cf674be37f90c9d34a36
Joshua <joshua.munsters@student.fontys.nl>
2018-05-27 19:04:33 +0200

    appdbcontext.cs gemaakt en startup.cs bijgewerkt

---
+++ HondenAsiel/HondenAsiel/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HondenAsiel.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Honden> honden { get; set; }
+        public DbSet<Ras> ras { get; set; }
+    }
+}
--- HondenAsiel/HondenAsiel/Startup.cs
+++ HondenAsiel/HondenAsiel/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using HondenAsiel.Data.Interfaces;
 using HondenAsiel.Data.Mocks;
+using Microsoft.Extensions.Configuration;
+using HondenAsiel.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HondenAsiel
 {
@@ -15,11 +18,24 @@ namespace HondenAsiel
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            //Server configuration
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
             services.AddTransient<IHondenRepo, MockHondenRepo>();
             services.AddTransient<IRasRepo, MockRasRepo>();
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
