946bff1d508c216be2cde0639384a54d7c2bcc63
Joshua <joshua.munsters@student.fontys.nl>
2018-05-28 16:08:58 +0200

    DB INITIALIZER GEMAAKT
    deze keer nog zonder startup.cs te bewerken want ik krijg errors

---
+++ HondenAsiel/HondenAsiel/Data/Models/DbInitializer.cs
@@ -0,0 +1,92 @@
+﻿using HondenAsiel.Data.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HondenAsiel.Data
+{
+    public class DbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            AppDbContext context =
+                applicationBuilder.ApplicationServices.GetRequiredService<AppDbContext>();
+
+            if (!context.ras.Any())
+            {
+                context.ras.AddRange(ras.Select(c => c.Value));
+            }
+
+            if (!context.honden.Any())
+            {
+                context.AddRange
+                (
+                    new Honden
+                    {
+                        Naam = "Luna",
+                        Prijs = 250,
+                        Ras = RAS["Labrador"]
+
+                    },
+                    new Honden
+                    {
+                        Naam = "PIETER",
+                        Prijs = 2500,
+                        Ras = RAS["Labrador"]
+
+                    },
+                    new Honden
+                    {
+                        Naam = "berend",
+                        Prijs = 22,
+                        Ras = RAS["Labrador"]
+                    },
+                    new Honden
+                    {
+                        Naam = "Rex",
+                        Prijs = 300,
+                        Ras = RAS["Labrador"]
+
+                    },
+                    new Honden
+                    {
+                        Naam = "dnhond",
+                        Prijs = 99999,
+                        Ras = RAS["Labrador"]
+
+                    }
+
+
+                );
+            }
+
+            context.SaveChanges();
+        }
+
+        private static Dictionary<string, Ras> RAS;
+        public static Dictionary<string, Ras> ras
+        {
+            get
+            {
+                if (RAS == null)
+                {
+                    var genresList = new Ras[]
+                    {
+                        new Ras { Rasnaam = "Labrador", Beschrijving="Mooie Hond" },
+                        new Ras { Rasnaam = "Poedel", Beschrijving="Gekke doggo" }
+                    };
+
+                    RAS = new Dictionary<string, Ras>();
+
+                    foreach (Ras genre in genresList)
+                    {
+                        RAS.Add(genre.Rasnaam, genre);
+                    }
+                }
+
+                return RAS;
+            }
+        }
+    }
+}
