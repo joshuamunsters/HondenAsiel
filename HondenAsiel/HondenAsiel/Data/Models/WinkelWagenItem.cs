f10c2a6d0eedd5127172d0456dec65413843807d
Joshua <joshua.munsters@student.fontys.nl>
2018-05-29 10:26:49 +0200

    WinkelWagenItem model gemaakt
    De winkelwagenitems die in de winkelwagen gaan

---
+++ HondenAsiel/HondenAsiel/Data/Models/WinkelWagenItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.Data.Models
+{
+    public class WinkelWagenItem
+    {
+        public int WinkelWagenItemId { get; set; }
+
+        public Honden Honden { get; set; }
+
+        public int Hoeveelheid { get; set; }
+
+        public string WinkelWagenId { get; set; }
+
+    }
+}
