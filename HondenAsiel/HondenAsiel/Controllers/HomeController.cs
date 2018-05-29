9a0b1baff3e1ddc8944103fdb5fa913c2c85f39a
Joshua <joshua.munsters@student.fontys.nl>
2018-05-29 14:23:04 +0200

    Homecontroller en bijbehorende views /viewmodels gemaakt
    Op de home pagina worden de meeste gewilde honden getoond. Ook heb ik Carousel.cshtml aangemaakt voor vormgeving (bootstrap)

---
+++ HondenAsiel/HondenAsiel/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HondenAsiel.Data.Interfaces;
+using HondenAsiel.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HondenAsiel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHondenRepo _hondenRepo;
+        public HomeController(IHondenRepo hondenRepo)
+        {
+            _hondenRepo = hondenRepo;
+        }
+        // GET: /<controller>/
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                GewildeHonden = _hondenRepo.Gewildehonden
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ HondenAsiel/HondenAsiel/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HondenAsiel.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Honden> GewildeHonden { get; set; }
+
+    }
+}
