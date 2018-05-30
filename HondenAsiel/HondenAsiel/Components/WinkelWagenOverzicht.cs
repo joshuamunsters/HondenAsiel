4a1f322ddaf5c59545673b4195fc5782f0817383
Joshua <joshua.munsters@student.fontys.nl>
2018-05-30 11:18:22 +0200

    Components/Viewcomponents toegevoegd en navbar aan layout toegevoegd

---
+++ HondenAsiel/HondenAsiel/Components/WinkelWagenOverzicht.cs
@@ -0,0 +1,35 @@
+﻿using HondenAsiel.Data.Models;
+using HondenAsiel.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.Components
+{
+    public class WinkelWagenOverzicht:ViewComponent
+    {
+        private readonly WinkelWagen _winkelWagen;
+
+        public WinkelWagenOverzicht(WinkelWagen winkelWagen)
+        {
+
+            _winkelWagen = winkelWagen;
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _winkelWagen.GetShoppingCartItems();
+            _winkelWagen.WinkelWagenItems = items;
+
+            var winkelWagenViewModel = new WinkelWagenViewModel
+            {
+                WinkelWagen = _winkelWagen,
+                WinkelWagenTotaal = _winkelWagen.GetShoppingCartTotal()
+            };
+            return View(winkelWagenViewModel);
+        }
+    }
+}
