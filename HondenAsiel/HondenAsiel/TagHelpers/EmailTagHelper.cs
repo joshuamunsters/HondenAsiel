f1698c97a71da9e92e2b80617353d6d2ea0fc6be
Joshua <joshua.munsters@student.fontys.nl>
2018-05-30 12:03:56 +0200

    TagHelper toegevoegd
    EmailTagHelper

---
+++ HondenAsiel/HondenAsiel/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.TagHelpers
+{
+    public class EmailTagHelper:TagHelper
+    {
+        public string Adress { get; set; }
+
+        public string Bericht { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Adress);
+            output.Content.SetContent(Bericht);
+        }
+
+    }
+}
