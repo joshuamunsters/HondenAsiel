cf3177da69dca7be3479cb389ba57403ea209c6a
Joshua <joshua.munsters@student.fontys.nl>
2018-05-31 14:46:29 +0200

    Menu gemaakt waar je honden kan orderen op ras

---
+++ HondenAsiel/HondenAsiel/Components/RasMenu.cs
@@ -0,0 +1,26 @@
+﻿using HondenAsiel.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.Components
+{
+    public class RasMenu:ViewComponent
+    {
+        private readonly IRasRepo _rasRepo;
+
+        public RasMenu(IRasRepo rasRepo)
+        {
+            _rasRepo = rasRepo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var rassen = _rasRepo.Rassen.OrderBy(p => p.Rasnaam);
+            return View(rassen);
+        }
+
+    }
+}
