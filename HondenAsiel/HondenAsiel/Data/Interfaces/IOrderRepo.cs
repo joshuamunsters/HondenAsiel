4636691ee7bc83f3ef1b92f54916951288fdae66
Joshua <joshua.munsters@student.fontys.nl>
2018-06-01 12:32:57 +0200

    Orderrepo en Orderinterface gemaakt

---
+++ HondenAsiel/HondenAsiel/Data/Interfaces/IOrderRepo.cs
@@ -0,0 +1,14 @@
+﻿using HondenAsiel.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondenAsiel.Data.Interfaces
+{
+    public interface IOrderRepo
+    {
+        void MaakOrder(Order order);
+
+    }
+}
---
+++ HondenAsiel/HondenAsiel/Data/Repositories/OrderRepo.cs
@@ -0,0 +1,44 @@
+﻿using HondenAsiel.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HondenAsiel.Data.Models;
+
+namespace HondenAsiel.Data.Repositories
+{
+    public class OrderRepo : IOrderRepo
+    {
+
+        private readonly AppDbContext _appDbContext;
+        private readonly WinkelWagen _winkelWagen;
+
+
+        public OrderRepo(AppDbContext appDbContext, WinkelWagen winkelWagen)
+        {
+            _appDbContext = appDbContext;
+            _winkelWagen = winkelWagen;
+        }
+
+        public void MaakOrder(Order order)
+        {
+            order.Ordergeplaatst = DateTime.Now;
+            _appDbContext.orders.Add(order);
+
+            var winkelwagenitems = _winkelWagen.WinkelWagenItems;
+
+            foreach (var item in winkelwagenitems)
+            {
+                var orderdeatails = new OrderDetails()
+                {
+                    Hoeveelheid = item.Hoeveelheid,
+                    HondenId = item.Honden.HondId,
+                    OrderId = order.OrderId,
+                    Prijs = item.Honden.Prijs
+                };
+
+                _appDbContext.orderdetails.Add(orderdeatails);
+            }
+        }
+    }
+}
